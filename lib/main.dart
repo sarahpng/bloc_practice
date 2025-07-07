7d4fafd90f0ac844955802824cf3751fa70c1779
sarahpng <sarahwaseem21@gmail.com>
2025-07-07 16:33:39 +0500

    bloc counter practice

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bloc_practice/bloc/counter/counter_bloc.dart';
+import 'package:bloc_practice/screens/counter_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const CounterScreen(),
+      ),
+    );
+  }
+}
